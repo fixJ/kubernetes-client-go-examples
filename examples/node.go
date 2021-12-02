428fe46f9ae3da9d4411dd7d44accf2d19a7ef3a
jx g <jxbebetter@gmail.com>
2021-12-02 17:50:46 +0800

    feat: list node

--- examples/node.go
+++ examples/node.go
@@ -2,6 +2,7 @@ package examples
 
 import (
 	"context"
+	coreV1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -24,4 +25,22 @@ func GetNodes(name string) error {
 	}
 	log.Printf("get node: %s successfully, node: %s", name, get)
 	return nil
+}
+
+func ListNodes() (*coreV1.NodeList, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	list, err := client.CoreV1().Nodes().List(context.TODO(), metaV1.ListOptions{})
+	if err != nil {
+		log.Printf("list node failed")
+		return nil, err
+	}
+	log.Printf("list node success: %v", list)
+	return list, nil
 }
